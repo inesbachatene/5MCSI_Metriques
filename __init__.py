7b790e474814fbfcf0bad869c2e865a35c52e70c
inesbachatene <159757350+inesbachatene@users.noreply.github.com>
2024-02-12 10:25:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,10 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
 
+@app.route("/commits/")
+def quantitecommits():
+    return render_template("Commit.html")
+
   
 if __name__ == "__main__":
   app.run(debug=True)
